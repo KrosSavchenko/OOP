e45150395fd03b90615709f5ab16114e40db9146
KrosSavchenko <KrosSavchenko@users.noreply.github.com>
2023-10-05 05:18:53 +0000

    Deploying to gh-pages from @ KrosSavchenko/OOP@71917f804d8b3856f5246dfa415739e2e2e4712e 🚀

--- Task_1_1_2/type-search-index.js
+++ Task_1_1_2/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.savchenko","l":"Main"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.savchenko","l":"Polynomial"}]
