22dd470de75541ece6a1ee70dc1025f26cc86e7b
KrosSavchenko <KrosSavchenko@users.noreply.github.com>
2023-10-17 17:54:17 +0000

    Deploying to gh-pages from @ KrosSavchenko/OOP@0b237b71ca652b99a5df170b12ac7578c08fdc0a 🚀

--- Task_1_2_1/member-search-index.js
+++ Task_1_2_1/member-search-index.js
@@ -1,1 +1,1 @@
-memberSearchIndex = [{"p":"org.example","c":"Main","l":"Main()","url":"%3Cinit%3E()"},{"p":"org.example","c":"Main","l":"main(String[])","url":"main(java.lang.String[])"}]
+memberSearchIndex = [{"p":"org.example","c":"Tree","l":"Tree(T)","url":"%3Cinit%3E(T)"}]
