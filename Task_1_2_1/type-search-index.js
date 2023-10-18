cda5fbf961056d91dfa51807377c2de8bb367248
KrosSavchenko <KrosSavchenko@users.noreply.github.com>
2023-10-18 10:51:27 +0000

    Deploying to gh-pages from @ KrosSavchenko/OOP@562237406956a435da2b01230710ad5143bc6009 🚀

--- Task_1_2_1/type-search-index.js
+++ Task_1_2_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"Tree"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"Tree"},{"p":"org.example","l":"Tree.TypeFirstSearch"}]
