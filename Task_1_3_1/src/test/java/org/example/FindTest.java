d2caa312a127961d6d6c017c1f7668ccd8fb0f10
Denis <d.savchenko@g..nsu.ru>
2023-11-08 20:33:57 +0700

    start

---
+++ Task_1_3_1/src/test/java/org/example/FindTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Class for tests.
+ */
+class FindTest {
+    @Test
+    public void testEquals() {
+        Assertions.assertEquals(1, 1 + 1);
+    }
+}
