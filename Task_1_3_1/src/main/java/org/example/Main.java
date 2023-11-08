b72333fc522c43110cdd2137d0dc4436e4644813
Denis <d.savchenko@g..nsu.ru>
2023-11-08 23:47:06 +0700

    vers1

--- Task_1_3_1/src/main/java/org/example/Main.java
+++ Task_1_3_1/src/main/java/org/example/Main.java
@@ -1,24 +1,38 @@
 package org.example;
 
-import java.io.*;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
+/**
+ * Main class with method find.
+ */
 public class Main {
     public static void main(String[] args) {
         ArrayList<Integer> arr = find("den.txt", "d");
         System.out.println(arr.toString());
     }
+
+    /**
+     * Method for finding all occurrences of a substring in a file.
+     *
+     * @param name File name.
+     * @param substring The substring that is being searched.
+     * @return An array with indexes of the occurrence of the substring.
+     */
     public static ArrayList<Integer> find(String name, String substring) {
         ArrayList<Integer> res = new ArrayList<>();
         int lens = substring.length();
-        try(FileReader reader = new FileReader(name)) {
+
+        try (FileReader reader = new FileReader(name)) {
             int c;
             int i = 0;
             if ((c = reader.read()) == -1) {
                 res.add(-1);
                 return res;
             }
-            while(true){
+
+            while (true) {
                 if (c == substring.codePointAt(0)) {
                     boolean isEnd = false;
                     boolean isSubString = true;
@@ -50,8 +64,7 @@ public class Main {
                     return res;
                 }
             }
-        }
-        catch(IOException ex) {
+        } catch(IOException ex) {
             System.out.println(ex.getMessage());
         }
 
